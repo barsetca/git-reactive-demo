dcf88f76afd48cacb4bde4d1f943a83812228195
chernyak <chernyak@protei-lab.ru>
2025-06-23 12:07:47 +0200

    init commit: reactive hello service

---
+++ src/main/java/com/vichernick/gitreactivedemo/GitReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vichernick.gitreactivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitReactiveDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitReactiveDemoApplication.class, args);
+    }
+
+}
